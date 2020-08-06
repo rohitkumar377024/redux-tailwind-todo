d5b2b48f72302cec1c037d4d042293c4f1fbf237
Rohit <rohitkumar377024@gmail.com>
2020-08-06 06:59:33 +0530

    Styled Buttons and Input Field for Todos List

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import Button from './components/Button';
+
 function App() {
-  return <div>Redux Tailwind Todo</div>;
+  return (
+    <div className="flex flex-col items-center mt-6">
+      <div>
+      <input
+        class="border border-gray-300 rounded px-6 py-1 mr-2"
+        type="text"
+        placeholder="Enter your task."
+      />
+      <Button size="sm" textColor="white" bgColor="blue-500">Add</Button>
+      </div>
+
+      <div class="text-lg px-3 py-3 mt-2">
+        <p className='inline-block mr-6'>Task 1</p>
+        <Button size="sm" textColor="white" bgColor="yellow-500">Update</Button>
+        <Button size="sm" textColor="white" bgColor="red-500">Delete</Button>
+      </div>
+    </div>
+  );
 }
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-template-curly-in-string */
+import React from 'react';
+import classnames from 'classnames';
+
+const Button = ({ size, bgColor, textColor, children }) => {
+  return (
+    <button
+      className={classnames(
+        `bg-${bgColor} text-${textColor} font-bold py-2 px-4 rounded mr-2`,
+        {
+          'text-xs': size === 'sm',
+          'text-xl': size === 'lg'
+        }
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
